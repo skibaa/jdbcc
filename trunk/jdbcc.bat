fa78acb3e214fa0df24215ed57fb09d2565079fa
skibaa <skibaa@58a72225-0e56-0410-b6c7-43480890602d>
2008-09-17 08:24:26 +0000

    handle spaces in pathnames in windows batch

--- trunk/jdbcc.bat
+++ trunk/jdbcc.bat
@@ -1,3 +1,3 @@
 @echo off
-%JAVA_HOME%\bin\java -jar %USERPROFILE%\.m2\repository\tv\bee\jdbcc\1.0.1\jdbcc-1.0.1.jar %*
+"%JAVA_HOME%\bin\java" -jar "%USERPROFILE%\.m2\repository\tv\bee\jdbcc\1.0.1\jdbcc-1.0.1.jar" %*
 
